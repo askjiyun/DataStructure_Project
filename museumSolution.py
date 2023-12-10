4994a5b8038ca686d3c318b0f6aa3e7e94a787da
askjiyun <104126233+askjiyun@users.noreply.github.com>
2023-12-11 01:30:53 +0900

    Update museumSolution.py

--- museumSolution.py
+++ museumSolution.py
@@ -1,5 +1,5 @@
 import itertools
-from museum import dijkstra_3d, exhibition_rooms, graph, room_to_floor_and_elevator
+from museum import dijkstra, exhibition_rooms, graph, room_to_floor_and_elevator
 
 # 층별 전시실 및 엘리베이터 정보
 room_to_floor_and_elevator = {
@@ -43,14 +43,14 @@ def find_shortest_path(graph, exhibition_rooms, start_point, end_point, selected
             if room_to_floor_and_elevator[current_point][0] != room_to_floor_and_elevator[node][0]:
                 # 같은 층에 없는 경우, 엘리베이터를 이용
                 elevator = room_to_floor_and_elevator[node][1]
-                distance, _ = dijkstra_3d(graph, current_point, elevator)
+                distance, _ = dijkstra(graph, current_point, elevator)
                 total_distance += distance if distance is not None else float('infinity')
                 current_point = elevator
                 if elevator not in current_path:
                     current_path.append(elevator)
 
             # 이제 해당 방으로 이동
-            distance, _ = dijkstra_3d(graph, current_point, node)
+            distance, _ = dijkstra(graph, current_point, node)
             if distance is None:  # 경로가 존재하지 않으면 계산을 중단
                 break
             total_distance += distance
@@ -58,7 +58,7 @@ def find_shortest_path(graph, exhibition_rooms, start_point, end_point, selected
             if node not in current_path:
                 current_path.append(node)
         else:
-            distance, _ = dijkstra_3d(graph, current_point, end_point)
+            distance, _ = dijkstra(graph, current_point, end_point)
             if distance is not None:
                 total_distance += distance
             if total_distance < shortest_distance:
@@ -71,7 +71,7 @@ def find_shortest_path(graph, exhibition_rooms, start_point, end_point, selected
 # 사용자 입력 받기
 start_room = input("관람을 시작할 전시실을 입력해주세요: ")
 end_room = input("관람을 마칠 전시실을 입력해주세요: ")
-selected_facilities = input("관람하고 싶은 편의시설을 입력하세요 (쉼표로 구분): ")
+selected_facilities = input("방문하고 싶은 편의시설을 입력하세요 (쉼표로 구분): ")
 
 # 최단 거리와 경로 계산
 shortest_distance, shortest_path = find_shortest_path(graph, exhibition_rooms, start_room, end_room, selected_facilities)
